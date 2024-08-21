7587c87887fb32fa8b08ea45a00aff0a28b0adc6
Swagatika637 <swagatikap624@gmail.com>
2024-08-21 10:09:54 +0530

    push1

---
+++ string.py
@@ -0,0 +1,13 @@
+str1="swaga"
+str2="tika"
+print(str1+str2)
+
+#length of string
+str1="swagatika"
+len1=len(str1)
+print(len1)
+ # type: ignore
+
+str2="abhi"
+len2=len(str2)
+print(len2)
---
+++ tuple.py
@@ -0,0 +1,28 @@
+tup = (10,20,394,"kujhggy,9+9j,True")
+print(type(tup))
+
+tu = (2,3,1)
+print(type(tu))
+
+tup = (1,3,6,4,9,7)
+print(tup[3])
+print(tup[2])            #print(tup[index value])
+
+tup = ()
+print(type(tup))
+
+tup = (1)
+print(tup)
+print(type(tup))
+
+tup = ("swagatika")
+print(tup)
+print(type(tup))
+
+
+#tuple method
+
+tup = ("swagatika")
+print(tup.index("g"))         
+print(tup.index("g"))         #index method
+print(tup.count("a"))         #count method
